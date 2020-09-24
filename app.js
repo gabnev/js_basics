87eb3c947272f89b8a9d443cf39e9aaf020998f2
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-24 11:34:02 +0100

    string methods & concatenation

--- app.js
+++ app.js
@@ -149,3 +149,33 @@ console.log(sum);
 console.log(typeof sum);
 
 console.clear();
+
+const num1 = 100;
+const num2 = 50;
+let value;
+
+value = num1 + num2;
+value = num1 - num2;
+value = num1 * num2;
+value = num1 / num2;
+value = num1 % num2;
+
+// Math Object - objects have properties AND methods (functions)
+
+value = Math.PI;
+value = Math.E;
+value = Math.round(2.4);
+value = Math.ceil(2.4);
+value = Math.floor(2.8);
+value = Math.sqrt(64);
+value = Math.abs(-3);
+value = Math.pow(8, 2); //pow = power of something
+value = Math.min(2,3,4,5); //return the minimum number value
+value = Math.max(2,3,4,5); //return the max number value
+value = Math.random();
+value = Math.random() * 20 + 1;
+value = Math.floor(Math.random() * 20 + 1);
+
+console.log(value);
+
+console.clear();
