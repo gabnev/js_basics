d7ddd07c9568a29f47bf831cbec52d830ea20613
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-24 11:17:59 +0100

    types conversion

--- app.js
+++ app.js
@@ -105,3 +105,47 @@ console.log(typeof objectLit);
 console.log(typeof date);
 
 console.clear();
+
+// Type Conversion
+
+let val;
+
+// num to string
+
+val = String(555);
+val = String(4+4);
+val = String(true);
+val = String(new Date());
+val = String([1,2,3,4,5]);
+
+val = (5).toString();
+val = (true).toString();
+
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1,2,3,4]);
+
+val = parseInt('100');
+val = parseFloat('100.30');
+
+
+
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed(2));
+
+// type cohersion
+// JS automatically converts the 6 to a string to allow the operation.
+
+const val1 = String(5);
+const val2 = 6;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
+
+console.clear();
