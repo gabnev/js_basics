821e439ffd6a0f37e5080c90b14d5e5415d76cf8
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-25 15:29:44 +0100

    finished basics

--- app.js
+++ app.js
@@ -767,3 +767,35 @@ value5 = window.navigator.vendor;
 value5 = window.navigator.language;
 
 console.log(value5);
+
+console.clear();
+
+// block scopes
+
+//var is bad because it allows changing global variables if repeated inside blocks
+
+var a = 1;
+let b = 2;
+const c = 3;
+
+// function test(){
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('Function Scope: ', a, b, c);
+// }
+
+// test();
+
+// if(true){
+//   var a = 7;
+//   let b = 8;
+//   const c = 9;
+//   console.log('If Scope: ', a, b, c);
+// }
+
+for(let a = 0; a < 10; a++){
+  console.log(`loop: ${a}`);
+}
+
+console.log('Global Scope: ', a, b, c);
